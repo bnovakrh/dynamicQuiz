906566f96b8be5cd4762b423dbeeae09d9335033
bnovakrh <bnovak@redhat.com>
2020-07-27 10:34:31 -0700

    Validation Logic

--- static/js/script.js
+++ static/js/script.js
@@ -1447,5 +1447,9 @@ function nextQuestion() {
       userScore();
     }
   }
-
+  function isEmail(firstName) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(firstName);
+  }
+  
 }
